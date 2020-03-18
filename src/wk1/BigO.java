8cecb0768444ad0bf6193a08a4d4f3bffa322394
Tyler Clark <tylerc.thedeveloper@gmail.com>
2020-03-18 17:20:28 -0400

    some updates to BigO

--- src/wk1/BigO.java
+++ src/wk1/BigO.java
@@ -3,33 +3,32 @@ package wk1;
 public class BigO {
 
 	public static void main(String[] args) {
-		compareFunctions(25);
+		compareFunctions(-100,120);
 
 	}
 
 	// this method represents n^2 (ignoring the constant)
-	private static int quadratic(int n) {
-		return (int) Math.pow(n, 2) + 500;
+	private static double quadratic(int n) {
+		return Math.pow(n, 2) + 500;
 
 	}
 
 	// this method represents 2^n
-	private static int exponential(int n) {
-		return (int) Math.pow(2, n);
+	private static double exponential(int n) {
+		return Math.pow(2, n - 100);
 	}
 
-	public static void compareFunctions(int n) {
-		for (int i = 0; i < n; i++) {
-			System.out.print("n:" + i + " ");
-			System.out.print("f1:" + quadratic(i) + " ");
-			System.out.print("f2:" + exponential(i) + " ");
+	public static void compareFunctions(int start, int stop) {
+		
+		for (int i = start; i < stop; i++) {
+			
+			System.out.printf("n: %d quadratic: %.1f exponential %f ", i, quadratic(i), exponential(i));
 			if (exponential(i) > quadratic(i)) {
-				System.out.print("f1 < f2\n");
+					System.out.print("f1 < f2\n");
+					
 			} else {
-				System.out.print("f1 > f2\n");
+					System.out.print("f1 > f2\n");
 			}
-
 		}
-
 	}
 }
