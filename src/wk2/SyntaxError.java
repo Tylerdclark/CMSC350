eac2ac6dccec41310422237aa7d4ee8fff39da61
Tyler Clark <tylerc.thedeveloper@gmail.com>
2020-03-25 23:22:59 -0400

    comments for the exception class

--- src/wk2/SyntaxError.java
+++ src/wk2/SyntaxError.java
@@ -1,3 +1,10 @@
+
+/* File: SyntaxError.java
+ * Date: 20 March 2020
+ * Author: Tyler D Clark
+ * Description: This file creates an exception which is to be thrown when operations are attempted on empty stacks or
+ * when the stack has elements left after.
+ */
 package wk2;
 
 public class SyntaxError extends Throwable {
