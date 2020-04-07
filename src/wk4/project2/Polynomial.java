a9be1a10228d194bbb28adc7923588fde3421ba1
Tyler Clark <tylerc.thedeveloper@gmail.com>
2020-04-07 01:59:29 -0400

    Beginning work on polynomial

--- src/wk4/project2/Polynomial.java
+++ src/wk4/project2/Polynomial.java
@@ -1,11 +1,15 @@
 package wk4.project2;
 
+import java.io.StreamTokenizer;
+import java.io.StringReader;
 import java.util.Iterator;
 
 public class Polynomial implements Iterable, Comparable {
 
     //todo: constructor - accepts a string that defines one polynomial in the same format as provided in the input file.
-    public Polynomial(){
+    public Polynomial(String fromfile){
+        //taking the String and splitting it into terms
+        StreamTokenizer tokenizeExpression = new StreamTokenizer(new StringReader(fromfile));
     }
 
     //todo: compareTo - that compares two polynomials.
@@ -25,6 +29,14 @@ public class Polynomial implements Iterable, Comparable {
     public String toString() {
         return super.toString();
     }
+
+    class Node { double coefficient; int exponent; Node next;
+
+        Node( double c, int e ) { coefficient = c; exponent = e; next = null;}
+    }
+
+
+
 }
 //todo: create static nested singly linked list class
 
