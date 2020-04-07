ce8b9b69badac112ad6ea6362dfb47df1614790d
Tyler Clark <tylerc.thedeveloper@gmail.com>
2020-04-07 01:59:41 -0400

    Can take in a file

--- src/wk4/project2/Project2.java
+++ src/wk4/project2/Project2.java
@@ -1,10 +1,62 @@
+/* File: Project2.java
+ * Date: 07 April 2020
+ * Author: Tyler D Clark
+ * Description: The description will go here :o) */
+
 package wk4.project2;
 
+import javax.swing.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+
 public class Project2 {
-    //todo: select file from JFileChooser
     //todo: populate an ArrayList with the Polynomial objects
     //todo: JOptionPane for invalid syntax
     //todo: strong order check
     //todo: weak order check
     //todo: named lambda expression implements comparable interface
+
+    public static void main(String[] args) {
+        // test
+        ArrayList<String> a = fromFile();
+        for (String element: a) {
+            System.out.println(element);
+        }
+    }
+
+    //===============================================================================================
+    //  method: fromFile /  returns: Arraylist / complexity: O(n)
+    //  description: Opens a file takes a String and tokenizes it into an ArrayList of of expressions
+    //===============================================================================================
+
+    public static ArrayList<String> fromFile() {
+        //Create ArrayList and JFileChooser
+        ArrayList<String> expressionList = new ArrayList<>();
+        JFileChooser chooser = new JFileChooser();
+        //Show both directories and files
+        chooser.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
+        //use current directory for ease
+        chooser.setCurrentDirectory(new File(System.getProperty("user.dir")));
+        int response = chooser.showOpenDialog(null);
+         if (response == JFileChooser.APPROVE_OPTION){
+             File file = chooser.getSelectedFile();
+             try {
+                 Scanner fileIn = new Scanner(file);
+                 if  (file.isFile()){
+                     while (fileIn.hasNextLine()){
+                         String expression = fileIn.nextLine();
+                         expressionList.add(expression);
+                     }
+                 }
+             }catch (NoSuchElementException nse){
+                System.err.println("File is empty!");
+             }catch(FileNotFoundException fne){
+                 System.err.println("File is not found!");
+             }
+         }
+        return expressionList;
+    }
 }
