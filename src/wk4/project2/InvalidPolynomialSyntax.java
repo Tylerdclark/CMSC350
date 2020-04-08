243038d751f332ba1a65b4add8f088d3d7c04197
Tyler Clark <tylerc.thedeveloper@gmail.com>
2020-04-08 00:53:47 -0400

    Constructor for exception

--- src/wk4/project2/InvalidPolynomialSyntax.java
+++ src/wk4/project2/InvalidPolynomialSyntax.java
@@ -1,6 +1,10 @@
 package wk4.project2;
 
-//todo: unchecked exception that contains a constructor that allows a message to be supplied
-
 public class InvalidPolynomialSyntax extends RuntimeException {
+    InvalidPolynomialSyntax(){
+        super();
+    }
+    InvalidPolynomialSyntax(String msg){
+        super(msg);
+    }
 }
