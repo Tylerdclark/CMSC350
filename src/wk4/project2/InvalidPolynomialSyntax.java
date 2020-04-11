207bb5991a1390520d65f33f4a1b3c35beff6979
Tyler Clark <tylerc.thedeveloper@gmail.com>
2020-04-11 17:42:38 -0400

    All I should need

--- src/wk4/project2/InvalidPolynomialSyntax.java
+++ src/wk4/project2/InvalidPolynomialSyntax.java
@@ -1,9 +1,6 @@
 package wk4.project2;
 
 public class InvalidPolynomialSyntax extends RuntimeException {
-    InvalidPolynomialSyntax(){
-        super();
-    }
     InvalidPolynomialSyntax(String msg){
         super(msg);
     }
