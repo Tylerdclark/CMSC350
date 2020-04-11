5827db474a904d64e14cb5ee1cecbc55f2fc3f2c
Tyler Clark <tylerc.thedeveloper@gmail.com>
2020-04-11 17:44:26 -0400

    Need to figure this out

--- src/wk4/project2/OrderedList.java
+++ src/wk4/project2/OrderedList.java
@@ -1,6 +1,16 @@
 package wk4.project2;
 
+import java.util.Comparator;
+import java.util.List;
+
 public class OrderedList {
     //todo: checkSorted - determines whether a List object, supplied as a parameter, is in strictly ascending order
     //todo: two methods (overloaded)
+    public static <T extends Comparable<? super T>> void checkSorted(List<T> list){
+
+    }
+
+    public static <T> void checkSorted(List<T> list, Comparator<? super T> c){
+
+    }
 }
