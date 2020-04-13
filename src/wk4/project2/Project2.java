27307d0a8b29e6b9201db94d03e6dc5eef4dd4dc
Tyler Clark <tylerc.thedeveloper@gmail.com>
2020-04-12 22:41:53 -0400

    put file polynomial processing in a method and make weak sort

--- src/wk4/project2/Project2.java
+++ src/wk4/project2/Project2.java
@@ -1,39 +1,28 @@
 /* File: Project2.java
  * Date: 07 April 2020
  * Author: Tyler D Clark
- * Description: The description will go here :o) */
+ * Description: This is the main class/driver of project2. This file reads a text file using JFileChooser and
+ * creates polynomial objects with each line in the text file. The Polynomial objects are places in a list which
+ * checks if the objects are in strong and weak order. */
 
 package wk4.project2;
 
 import javax.swing.*;
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.NoSuchElementException;
-import java.util.Scanner;
+import java.util.*;
 
 public class Project2 {
-    //todo: populate an ArrayList with the Polynomial objects
-    //todo: JOptionPane for invalid syntax
-    //todo: strong order check
-    //todo: weak order check
-    //todo: named lambda expression implements comparable interface
 
+    private static List<Polynomial> polyList = new ArrayList<>();
+
+    //main here
     public static void main(String[] args) {
-        // test
-        try {
-            ArrayList<String> a = fromFile();
-            for (String element : a) {
-                Polynomial p = new Polynomial(element);
-                System.out.println(p);
-            }
-        }catch (InvalidPolynomialSyntax ex){
-            JOptionPane.showMessageDialog(JOptionPane.getRootFrame(),ex.getMessage());
-        }
+        processPolyList();
     }
 
     //===============================================================================================
-    //  method: fromFile /  returns: Arraylist / complexity: O(n)
+    //  method: fromFile /  returns: Arraylist
     //  description: Opens a file takes a String and tokenizes it into an ArrayList of of expressions
     //===============================================================================================
 
@@ -57,11 +46,46 @@ public class Project2 {
                      }
                  }
              }catch (NoSuchElementException nse){
-                System.err.println("File is empty!");
+                JOptionPane.showMessageDialog(JOptionPane.getRootFrame(),"File is empty!");
              }catch(FileNotFoundException fne){
-                 System.err.println("File is not found!");
+                 JOptionPane.showMessageDialog(JOptionPane.getRootFrame(),"File is not found!");
              }
          }
         return expressionList;
+    }
+    //===============================================================================================
+    //  method: checkWeakOrder /  returns: boolean
+    //  description: determines if a list is in weak order (should be lambda expression)
+    //===============================================================================================
+    public static boolean checkWeakOrder( List<Polynomial> polyList){
+        boolean isWeakOrder = true;
+        Polynomial previous = polyList.get(polyList.size()-1);
+        for(int i = polyList.size()-2; i > 0; i--){
+
+           if (previous.compareExponents(polyList.get(i)) < 0){
+               isWeakOrder = false;
+           }
+        }
+        return isWeakOrder;
+    }
+    //===============================================================================================
+    //  method: processPolyList /  returns: void / catches: InvalidPolynomialSyntax
+    //  description: calls fromFile to fill a list with Polynomial objects and checks list order
+    //===============================================================================================
+    public static void processPolyList(){
+        try {
+            ArrayList<String> a = fromFile();
+            for (String element : a) {
+                Polynomial p = new Polynomial(element);
+                System.out.println(p);
+                polyList.add(p);
+            }
+        }catch (InvalidPolynomialSyntax ex){
+            JOptionPane.showMessageDialog(JOptionPane.getRootFrame(),ex.getMessage());
+        }
+        /*  Call to check sorted for the Strong order check    */
+        System.out.println("Strong Ordered: " + OrderedList.checkSorted(polyList));
+        /*  Check for Weak order (exponents only)    */
+        System.out.println("Weak Ordered: " + checkWeakOrder(polyList));
     }
 }
