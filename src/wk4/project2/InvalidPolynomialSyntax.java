628689c85e9098614b49af61bc4df3762b42a628
Tyler Clark <tylerc.thedeveloper@gmail.com>
2020-04-12 22:40:22 -0400

    documentation added and format

--- src/wk4/project2/InvalidPolynomialSyntax.java
+++ src/wk4/project2/InvalidPolynomialSyntax.java
@@ -1,7 +1,12 @@
+/* File: OrderedList.java
+ * Date: 07 April 2020
+ * Author: Tyler D Clark
+ * Description: An unchecked exception which is to be thrown when incorrect syntax is passed to the Polynomial
+ * constructor. */
+
 package wk4.project2;
 
 public class InvalidPolynomialSyntax extends RuntimeException {
-    InvalidPolynomialSyntax(String msg){
-        super(msg);
+    InvalidPolynomialSyntax(String msg){super(msg);
     }
 }
