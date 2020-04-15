50381a0394ae0231f7d370f68df345388da78d12
Tyler Clark <tylerc.thedeveloper@gmail.com>
2020-04-15 18:01:46 -0400

    Add on a nested node class

--- src/wk6/BinaryTree.java
+++ src/wk6/BinaryTree.java
@@ -1,4 +1,37 @@
 package wk6;
 
 public class BinaryTree {
+
+    public static class Node<T>{
+        private T info;
+        private Node<T> left;
+        private Node<T> right;
+
+        public Node(T info){
+            this.info = info;
+            left = null; //explicitly stating null
+            right = null;
+        }
+        public void setInfo(T info){
+            this.info = info;
+        }
+        public T getInfo(){
+            return info;
+        }
+        public void setLeft(Node<T> link){
+            left = link;
+        }
+        public void setRight(Node<T> link){
+            right = link;
+        }
+        public Node<T> getLeft(){
+            return left;
+        }
+        public Node<T> getRight(){
+            return right;
+        }
+    }
+
+
+
 }
