74ee85d31d62e915cf76aac1c0f556239634f665
Tyler Clark <tylerc.thedeveloper@gmail.com>
2020-04-15 16:04:40 -0400

    Checked exception for binary tree

---
+++ src/wk6/InvalidTreeSyntax.java
@@ -0,0 +1,17 @@
+/* File: InvalideTreeSyntax.java
+ * Date: 15 April 2020
+ * Author: Tyler D Clark
+ * Description: This file creates an exception which is to be thrown when an invalid String is passed to the constructor
+ * from the project3 GUI
+ */
+package wk6;
+
+public class InvalidTreeSyntax extends Exception{
+
+    public InvalidTreeSyntax(){
+        super();
+    }
+    public InvalidTreeSyntax(String message){
+        super(message);
+    }
+}
