e0c1fa6d577d2725f729f0a9824f885e087d9501
Tyler Clark <tylerc.thedeveloper@gmail.com>
2020-04-21 18:00:41 -0400

    Significant work to the gui

--- src/wk6/Project3.java
+++ src/wk6/Project3.java
@@ -8,10 +8,15 @@ import java.awt.event.ActionListener;
 
 public class Project3 extends JFrame {
 
-    private JTextField input = new JTextField(20), output = new JTextField(20);
+    private JTextField input = new JTextField(20), output = new JTextField(30);
     private final JButton makeTree = new JButton("Make Tree");
     private final JButton isBalanced = new JButton("Is Balanced?");
     private final JButton isFull = new JButton("Is Full?");
+    private final JButton isProper = new JButton("Is Proper?");
+    private final JButton height = new JButton("Height");
+    private final JButton nodes = new JButton("Nodes");
+    private final JButton inOrder = new JButton("Inorder");
+    private static BinaryTree inputTree;
 
     public static void main(String[] args) {
         Project3 frame = new Project3();
@@ -21,20 +26,21 @@ public class Project3 extends JFrame {
 
     public Project3() {
         super("Binary Tree Categorizer");
-        setSize(400, 175);
+        setSize(715, 175);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLayout(new GridLayout(3, 1));
         JComponent[] inputComponents = {new JLabel("Enter Expression"),
                 input};
         makeFlowPanel(inputComponents);
-        JComponent[] buttonComponents = {makeTree, isBalanced, isFull };
+        JComponent[] buttonComponents = {makeTree, isBalanced, isFull, isProper, height, nodes, inOrder };
         makeFlowPanel(buttonComponents);
         JComponent[] outputComponents = {new JLabel("Result"), output};
         makeFlowPanel(outputComponents);
         output.setEditable(false);
-        makeTree.addActionListener(treeMaker);
-        isBalanced.addActionListener(checkBalanced);
-        isFull.addActionListener(checkBalanced);
+        makeTree.addActionListener(treeListener);
+        isBalanced.addActionListener(treeListener);
+        isFull.addActionListener(treeListener);
+        isProper.addActionListener(treeListener);
     }
 
     private void makeFlowPanel(JComponent[] components) {
@@ -44,23 +50,36 @@ public class Project3 extends JFrame {
             panel.add(component);
         add(panel);
     }
-
-
-
-    // TODO: maybe just one action listener for all of the buttons w/ if statement
-    private final ActionListener treeMaker = event -> {
+    
+    private final ActionListener treeListener = event -> {
         try {
+            switch ((event.getActionCommand())){
+                case "Make Tree":
+                    inputTree = new BinaryTree(input.getText());
+                    output.setText(inputTree.toString());
+                    break;
+                case "Is Balanced?":
+                    System.out.println(inputTree.checkBalanced());
+                    break;
+                case "Is Full?":
+                    System.out.println(inputTree.checkFull());
+                    break;
+                case "Is Proper?":
+                    System.out.println(inputTree.isProper());
+                    break;
+                case "Height":
+                    System.out.println(inputTree.height());
+                    break;
+                case "Nodes":
+                    System.out.println(inputTree.nodes());
+                    break;
+                case "Inorder":
+                    System.out.println(inputTree.inOrder());
+                    break;
+            }
             //get input / set output
         } catch (Exception except) {
-            JOptionPane.showMessageDialog(null, except.getMessage());
-        }
-    };
-
-    private final ActionListener checkBalanced = event -> {
-        try {
-            // get input / set output
-        }catch (Exception except){
-            JOptionPane.showMessageDialog(null, except.getMessage());
+            System.out.println(except.getMessage());
         }
     };
 }
