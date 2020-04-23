bb402557367745438f09426863f177b30c45d741
Tyler Clark <tylerc.thedeveloper@gmail.com>
2020-04-22 22:43:00 -0400

    Few method additions

--- src/wk6/BinaryTree.java
+++ src/wk6/BinaryTree.java
@@ -17,10 +17,9 @@ public class BinaryTree {
     public BinaryTree(String input) throws InvalidTreeSyntax {
 
         Stack<Node> nodeStack = new Stack<>();
-        String[] inputArray = input.substring(1, input.length()-1) // remove first and last parenthesis
+        String[] inputArray = input.substring(1, input.length()-1) // remove first parenthesis
                 //and split the String into a arr of strings, retain the parenthesis
                 .split("(?<=\\()|(?=\\()|(?<=\\))|(?=\\))");
-
 
         parent = new Node(inputArray[0]); //setting the first character to the root
 
@@ -31,23 +30,18 @@ public class BinaryTree {
                 if (child != null) {
                     parent = child;
                 }
-
             }else if(inputArray[i].equals(")")){
                 child = parent;
                 parent= nodeStack.pop();
-
             }else{
                 child = new Node(inputArray[i]);
                 if (parent != null) {
                     parent.addChild(child);
                 }
-
             }
             System.out.println(parent);
         }
     }
-
-
 
     public boolean checkBalanced() {
         return true;
@@ -62,23 +56,36 @@ public class BinaryTree {
     }
 
     public int height() {
-        return 0;
+        //subtract one since in this exercise, root is 0
+        return recHeight(this.parent) - 1;
+    }
+
+    private int recHeight(Node root) {
+        return (root == null) ? 0 : Math.max(recHeight(root.left), recHeight(root.right))+1;
+
     }
 
     public int nodes() {
-        return 0;
+        return recNodes(this.parent);
     }
 
-    public BinaryTree inOrder() {
-        return null;
+    private int recNodes(Node root) {
+        return (root == null) ? 0 : 1 + recNodes(root.left) + recNodes(root.right);
+
     }
 
+    public String inOrder() {
+        return recInOrder(this.parent);
+    }
+
+    private String recInOrder(Node root) {
+        return (root == null) ? "" : "("  + recInOrder(root.left) + root.info  + recInOrder(root.right) + ")";
+    }
+
+    // simply calls the the root node toString
     @Override
     public String toString() {
-        return "BinaryTree{" +
-                "parent=" + parent +
-                ", child=" + child +
-                '}';
+        return parent.toString();
     }
     //===============================================================================================
     //   Nested node class/ arguments: character info/ helper methods for nodes
@@ -93,26 +100,25 @@ public class BinaryTree {
         public Node(String info) { this.info = info; }
 
         public void addChild(Node child) throws InvalidTreeSyntax {
+
             if (this.getLeft() == null){ this.setLeft(child); }
-            else if (this.getRight() == null){ this.setRight(child); }
-            else{ throw new InvalidTreeSyntax("Problems");}
-        }
+            else if (this.getRight() == null){  this.setRight(child); }
+            else{ throw new InvalidTreeSyntax("Problems adding child");} }
 
-        public void setLeft(Node link) { left = link; }
-
-        public void setRight(Node link) { right = link; }
-
+        //Simple setter and getters for the nodes
+        public void setLeft(Node newLeft) { left = newLeft; }
+        public void setRight(Node newRight) { right = newRight; }
         public Node getLeft() { return left; }
-
         public Node getRight() { return right; }
 
+        //To call the recursive method
         @Override
         public String toString() {
-            return "Node{" +
-                    "info=" + info +
-                    ", left=" + left +
-                    ", right=" + right +
-                    '}';
+            return toString(this);
+        }
+        // recursively printing out the nodes
+        public static String toString(Node root) {
+            return (root == null) ? "" : "(" + root.info  + toString(root.left) + toString(root.right) + ")";
         }
     }
 }
