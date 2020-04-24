0af5a666eab3138b257f557e99d50bdf1c641e8d
Tyler Clark <tylerc.thedeveloper@gmail.com>
2020-04-23 21:53:16 -0400

    few changes

--- src/wk6/Project3.java
+++ src/wk6/Project3.java
@@ -4,8 +4,6 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionListener;
 
-//TODO: Scanner object to read from textfield String  and create tree object
-
 public class Project3 extends JFrame {
 
     private JTextField input = new JTextField(20), output = new JTextField(30);
@@ -30,20 +28,22 @@ public class Project3 extends JFrame {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLayout(new GridLayout(3, 1));
         JComponent[] inputComponents = {new JLabel("Enter Expression"), input};
-        makeFlowPanel(inputComponents);
         JButton[] buttonComponents = {makeTree, isBalanced, isFull, isProper, height, nodes, inOrder };
-        makeFlowPanel(buttonComponents);
         JComponent[] outputComponents = {new JLabel("Result"), output};
+        makeFlowPanel(inputComponents);
+        makeFlowPanel(buttonComponents);
         makeFlowPanel(outputComponents);
-        output.setEditable(false);
         addAL(buttonComponents);
+        output.setEditable(false);
+        setResizable(false);
     }
 
     private void makeFlowPanel(JComponent[] components) {
         JPanel panel = new JPanel();
         panel.setLayout(new FlowLayout());
-        for (Component component: components)
+        for (Component component: components) {
             panel.add(component);
+        }
         add(panel);
     }
     private void addAL (JButton[] buttons){
@@ -60,13 +60,13 @@ public class Project3 extends JFrame {
                     output.setText(inputTree.toString());
                     break;
                 case "Is Balanced?":
-                    System.out.println(inputTree.checkBalanced());
+                    output.setText(String.valueOf(inputTree.checkBalanced()));
                     break;
                 case "Is Full?":
-                    System.out.println(inputTree.checkFull());
+                    output.setText(String.valueOf(inputTree.checkFull()));
                     break;
                 case "Is Proper?":
-                    System.out.println(inputTree.isProper());
+                    output.setText(String.valueOf(inputTree.isProper()));
                     break;
                 case "Height":
                     output.setText(String.valueOf(inputTree.height()));
