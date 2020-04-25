d677eb5e0b33b8f0b3c240c34649f893576503ee
Tyler Clark <tylerc.thedeveloper@gmail.com>
2020-04-25 12:46:35 -0400

    Comments and refactoring

--- src/wk6/Project3.java
+++ src/wk6/Project3.java
@@ -1,58 +1,68 @@
+/* File: Project3.java
+ * Date: 24 April 2020
+ * Author: Tyler D Clark
+ * Description: The main file where the GUI is created and where the users are given the opportunity to enter inputs
+ * that represent Binary trees using the syntax: (parent(child1)(child2)). Note that there can be at most two
+ * children for a given parent and one root parent. */
+
 package wk6;
 
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionListener;
-import java.util.EmptyStackException;
 
 public class Project3 extends JFrame {
 
     private JTextField input = new JTextField(20), output = new JTextField(30);
-    private final JButton makeTree = new JButton("Make Tree");
-    private final JButton isBalanced = new JButton("Is Balanced?");
-    private final JButton isFull = new JButton("Is Full?");
-    private final JButton isProper = new JButton("Is Proper?");
-    private final JButton height = new JButton("Height");
-    private final JButton nodes = new JButton("Nodes");
-    private final JButton inOrder = new JButton("Inorder");
     private static BinaryTree inputTree;
 
+    //main here
     public static void main(String[] args) {
         Project3 frame = new Project3();
         frame.setVisible(true);
     }
 
-
+    //===================================================================================
+    // Class constructor creates GUI with three panels (input, buttons, and output)
+    //===================================================================================
     public Project3() {
         super("Binary Tree Categorizer");
         setSize(715, 175);
+        setLocationRelativeTo(null);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLayout(new GridLayout(3, 1));
         JComponent[] inputComponents = {new JLabel("Enter Expression"), input};
-        JButton[] buttonComponents = {makeTree, isBalanced, isFull, isProper, height, nodes, inOrder };
         JComponent[] outputComponents = {new JLabel("Result"), output};
+        JButton[] buttonComponents = {new JButton("Make Tree"), new JButton("Is Balanced?"),
+                new JButton("Is Full?"), new JButton("Is Proper?"), new JButton("Height"),
+                new JButton("Nodes"), new JButton("Inorder")};
         makeFlowPanel(inputComponents);
         makeFlowPanel(buttonComponents);
         makeFlowPanel(outputComponents);
-        addAL(buttonComponents);
+        addActionListeners(buttonComponents);
         output.setEditable(false);
         setResizable(false);
     }
 
+    //================================================================================================
+    // Creates a flow panel from array of components. Borrowed the idea for this method from Prof Jarc
+    //================================================================================================
     private void makeFlowPanel(JComponent[] components) {
-        JPanel panel = new JPanel();
-        panel.setLayout(new FlowLayout());
-        for (Component component: components) {
-            panel.add(component);
-        }
+        JPanel panel = new JPanel(new FlowLayout());
+        for (Component component: components) { panel.add(component); }
         add(panel);
     }
-    private void addAL (JButton[] buttons){
+    //================================================================================================
+    // Adds the ActionListener to the array of buttons passed to it, similar to previous
+    //================================================================================================
+    private void addActionListeners (JButton[] buttons){
         for (JButton button: buttons){
             button.addActionListener(treeListener);
         }
     }
-
+    //========================================================================================================
+    // ActionListener uses switch statement to set output text based on String returned from getActionCommand
+    //========================================================================================================
     private final ActionListener treeListener = event -> {
         try {
             switch ((event.getActionCommand())){
@@ -61,10 +71,10 @@ public class Project3 extends JFrame {
                     output.setText(inputTree.toString());
                     break;
                 case "Is Balanced?":
-                    output.setText(String.valueOf(inputTree.checkBalanced()));
+                    output.setText(String.valueOf(inputTree.isBalanced()));
                     break;
                 case "Is Full?":
-                    output.setText(String.valueOf(inputTree.checkFull()));
+                    output.setText(String.valueOf(inputTree.isFull()));
                     break;
                 case "Is Proper?":
                     output.setText(String.valueOf(inputTree.isProper()));
@@ -79,9 +89,10 @@ public class Project3 extends JFrame {
                     output.setText(inputTree.inOrder());
                     break;
             }
-            //get input / set output
-        } catch (IndexOutOfBoundsException | InvalidTreeSyntax except) {
-            JOptionPane.showMessageDialog(null,except.getMessage());
+        } catch (InvalidTreeSyntax except) {
+            JOptionPane.showMessageDialog(getParent(),except.getMessage());
+        }catch (IndexOutOfBoundsException indexExcept) {
+            JOptionPane.showMessageDialog(getParent(),"No input given!");
         }
     };
 }
