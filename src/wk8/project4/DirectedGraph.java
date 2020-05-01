5d05ba9ed80c69f1117a121cd029fa27fce33fad
Tyler Clark <tylerc.thedeveloper@gmail.com>
2020-04-30 23:55:30 -0400

    added nested classes for the vertex and the edge

--- src/wk8/project4/DirectedGraph.java
+++ src/wk8/project4/DirectedGraph.java
@@ -1,4 +1,15 @@
 package wk8.project4;
 
 public class DirectedGraph {
+
+
+    private static class Vertex <E>{
+        E element;
+        Edge<E> firstEdge;
+        Vertex<E> nextVertex;
+    }
+    private static class Edge<E>{
+        Vertex<E> vertex;
+        Edge<E> nextEdge;
+    }
 }
