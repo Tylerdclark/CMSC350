76d79277a9e77d8371867d1dc0cb9274a3a181a6
Tyler Clark <tylerc.thedeveloper@gmail.com>
2020-04-30 23:54:57 -0400

    Give a user choice for selecting a file

--- src/wk8/project4/Project4.java
+++ src/wk8/project4/Project4.java
@@ -1,18 +1,25 @@
 package wk8.project4;
 
 import javax.swing.*;
+import java.awt.*;
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
+import java.lang.reflect.InvocationTargetException;
 import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 public class Project4 {
 
-    public static void main(String[] args) {
-        fromFile();
+    public static void main(String[] args) throws InvocationTargetException, InterruptedException {
+        int userChoice = JOptionPane.showConfirmDialog(null, "Would you like to select a file?",
+                "Project 4", JOptionPane.YES_NO_CANCEL_OPTION);
+        while(userChoice == 0 ){
+            //JFileChooser showDialog was giving me issues on OSX, so this allowed it to execute in the right thread
+            EventQueue.invokeAndWait(Project4::fromFile);//chose method reference over lambda expression
+            userChoice = JOptionPane.showConfirmDialog(null, "Would you like to select a file?",
+                    "Project 4", JOptionPane.YES_NO_CANCEL_OPTION);
+        }
     }
-
     public static void fromFile() {
         //Create JFileChooser
         JFileChooser chooser = new JFileChooser();
@@ -20,7 +27,7 @@ public class Project4 {
         chooser.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
         //use current directory for ease
         chooser.setCurrentDirectory(new File(System.getProperty("user.dir")));
-        int response = chooser.showOpenDialog(null);
+        int response = chooser.showOpenDialog(chooser.getParent());
         if (response == JFileChooser.APPROVE_OPTION){
             File file = chooser.getSelectedFile();
             try {
@@ -39,6 +46,5 @@ public class Project4 {
                 JOptionPane.showMessageDialog(JOptionPane.getRootFrame(),"File is not found!");
             }
         }
-
     }
 }
