d2088f54ca11e5c3b63c5f157b6dbea0c8f459d2
Tyler Clark <tylerc.thedeveloper@gmail.com>
2020-05-06 17:04:12 -0400

    So far, so good, but I am getting duplicate vertices

--- src/wk8/project4/DirectedGraph.java
+++ src/wk8/project4/DirectedGraph.java
@@ -1,25 +1,21 @@
 package wk8.project4;
 
+//TODO: method to allow edges to be added to the graph
+//TODO: method to do a depth-first search
+//TODO: method to display unreachable vertices
 
 
 public class DirectedGraph<E> {
 
-    Vertex<E> firstVertex;
+    // Some instance variables
+    Vertex<E> firstVertex, lastVertex;
+    private static int vertices, edges;
+    public static int fileEntityCount;
 
 
-    public void addVertex(E label){
-
-        if (firstVertex == null){
-            firstVertex = new Vertex<>(label);
-        } else{
-            Vertex<E> current = firstVertex;
-            while(current.nextVertex != null){
-                current = current.nextVertex;
-            }
-            current.nextVertex = new Vertex<>(label);
-        }
-    }
-
+    //===============================================================================================
+    //  Static nested Vertex class.
+    //===============================================================================================
     private static class Vertex <E>{
         E element;
         Edge<E> firstEdge;
@@ -30,23 +26,14 @@ public class DirectedGraph<E> {
             element = label;
             discovered = false;
         }
+        public boolean hasNext(){ return (nextVertex != null); }
+        public Vertex<E> next(){ return nextVertex; }
 
 
-        public void addEdge(Edge<E> newEdge) {
-            Edge<E> current = firstEdge;
-            if (current == null) {
-                current = newEdge;
-                current.nextEdge = null;
-            } else {
-                while (current.nextEdge != null) {
-                    current = current.nextEdge;
-                }
-                current.nextEdge = newEdge;
-            }
-        }
-
-    }
-
+    } // end vertex class
+    //===============================================================================================
+    //  Static nested Edge class.
+    //===============================================================================================
     private static class Edge<E>{
         //the vertex that the edge connects to
         Vertex<E> vertex;
@@ -55,13 +42,88 @@ public class DirectedGraph<E> {
         public Edge(Vertex<E> v){
             vertex = v;
         }
-        public void setNextEdge(Edge<E> nextEdge) {
-            this.nextEdge = nextEdge;
+
+        public Edge<E> next(){ return nextEdge;}
+        public boolean hasNext() { return (nextEdge != null); }
+
+        public String toString(){
+            return vertex.element.toString();
+        }
+    } // end edge class
+
+
+
+    public void addVertex(E label){
+
+        if (firstVertex == null){
+            firstVertex = new Vertex<>(label);
+        } else{
+            Vertex<E> current = firstVertex;
+            while(current.hasNext()){
+                current = current.next();
+            }
+            current.nextVertex = new Vertex<>(label);
+            vertices++;
+        }
+    }
+
+    public void createEdge(E from, E to) {
+        // if the vertex doesnt exist, it gets made, otherwise returned
+        Vertex<E> fromVertex = findAddVertex(from);
+        Vertex<E> toVertex = findAddVertex(to);
+
+        AddEdge(fromVertex, toVertex);
+    }
+
+    private void AddEdge(Vertex<E> fromVertex, Vertex<E> toVertex) {
+        //what if the first edge is null?
+        if (fromVertex.firstEdge == null){ fromVertex.firstEdge = new Edge<>(toVertex); }
+        else{ Edge<E> currentEdge = fromVertex.firstEdge;
+            while(currentEdge.hasNext()){
+                currentEdge = currentEdge.next();
+            }currentEdge.nextEdge = new Edge<>(toVertex);
         }
 
     }
 
-    //TODO: method to allow edges to be added to the graph
-    //TODO: method to do a depth-first search
-    //TODO: method to display unreachable vertices
+
+    public Vertex<E> findAddVertex(E value) {
+        if (firstVertex != null) {
+            Vertex<E> current = firstVertex;
+            if (value == current.element) {
+                return current; //check first vertex
+            } else {
+                while (current.hasNext()) { // loop over all of the vertices
+                    if (value == current.element) {
+                        return current;
+                    } // if we find, return it
+                    else {
+                        current = current.next();
+                    } //update
+                }//if it gets here, it doesn't exist. Create it and recall this method
+                addVertex(value);
+                return findAddVertex(value);
+            }
+        } else{
+            addVertex(value);
+            return findAddVertex(value);
+        }
+    }
+
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        Vertex<E> currentVertex = firstVertex;
+        while (currentVertex != null) {
+            sb.append(currentVertex.element);
+
+            sb.append('\n');
+            currentVertex = currentVertex.next();
+        }
+
+
+        return sb.toString();
+    }
 }
+
