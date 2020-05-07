fec9fa3c981bdf90e9973eda69835d461c9428d5
Tyler Clark <tylerc.thedeveloper@gmail.com>
2020-05-06 23:58:08 -0400

    thats it for now.. still a lot to do

--- src/wk8/project4/DirectedGraph.java
+++ src/wk8/project4/DirectedGraph.java
@@ -7,15 +7,12 @@ package wk8.project4;
 
 public class DirectedGraph<E> {
 
-    // Some instance variables
-    Vertex<E> firstVertex, lastVertex;
-    private static int vertices, edges;
-    public static int fileEntityCount;
-
+    Vertex<E> firstVertex;
 
     //===============================================================================================
     //  Static nested Vertex class.
     //===============================================================================================
+
     private static class Vertex <E>{
         E element;
         Edge<E> firstEdge;
@@ -28,20 +25,18 @@ public class DirectedGraph<E> {
         }
         public boolean hasNext(){ return (nextVertex != null); }
         public Vertex<E> next(){ return nextVertex; }
+    }
 
-
-    } // end vertex class
     //===============================================================================================
     //  Static nested Edge class.
     //===============================================================================================
+
     private static class Edge<E>{
         //the vertex that the edge connects to
         Vertex<E> vertex;
         Edge<E> nextEdge;
 
-        public Edge(Vertex<E> v){
-            vertex = v;
-        }
+        public Edge(Vertex<E> v){ vertex = v; }
 
         public Edge<E> next(){ return nextEdge;}
         public boolean hasNext() { return (nextEdge != null); }
@@ -49,23 +44,11 @@ public class DirectedGraph<E> {
         public String toString(){
             return vertex.element.toString();
         }
-    } // end edge class
-
-
-
-    public void addVertex(E label){
-
-        if (firstVertex == null){
-            firstVertex = new Vertex<>(label);
-        } else{
-            Vertex<E> current = firstVertex;
-            while(current.hasNext()){
-                current = current.next();
-            }
-            current.nextVertex = new Vertex<>(label);
-            vertices++;
-        }
     }
+
+    //===============================================================================================
+    //  createEdge - either finds or creates the Vertices for which it will made an edge between
+    //===============================================================================================
 
     public void createEdge(E from, E to) {
         // if the vertex doesnt exist, it gets made, otherwise returned
@@ -75,6 +58,10 @@ public class DirectedGraph<E> {
         AddEdge(fromVertex, toVertex);
     }
 
+    //===============================================================================================
+    //  AddEdge - called by createEdge to do some of the work of traversing the linked lists
+    //===============================================================================================
+
     private void AddEdge(Vertex<E> fromVertex, Vertex<E> toVertex) {
         //what if the first edge is null?
         if (fromVertex.firstEdge == null){ fromVertex.firstEdge = new Edge<>(toVertex); }
@@ -83,47 +70,47 @@ public class DirectedGraph<E> {
                 currentEdge = currentEdge.next();
             }currentEdge.nextEdge = new Edge<>(toVertex);
         }
-
     }
 
+    //===============================================================================================
+    //  findAddVertex - attempts to find a vertex and return it, otherwise adds it and returns it
+    //===============================================================================================
 
+// v-----> This is clearly the problem area <-----v //
     public Vertex<E> findAddVertex(E value) {
-        if (firstVertex != null) {
-            Vertex<E> current = firstVertex;
-            if (value == current.element) {
-                return current; //check first vertex
-            } else {
-                while (current.hasNext()) { // loop over all of the vertices
-                    if (value == current.element) {
-                        return current;
-                    } // if we find, return it
-                    else {
-                        current = current.next();
-                    } //update
-                }//if it gets here, it doesn't exist. Create it and recall this method
-                addVertex(value);
-                return findAddVertex(value);
-            }
-        } else{
-            addVertex(value);
-            return findAddVertex(value);
+        if (firstVertex == null){
+            return firstVertex = new Vertex<>(value);
         }
-    }
+        Vertex <E> current = firstVertex;
+        while (current.hasNext()){
+            if (current.element == value){ return current; }
+            current = current.next();
+        }
+        return current.nextVertex = new Vertex<>(value);
+    }// ^-----> This is clearly the problem area <-----^ //
 
+    //===============================================================================================
+    //  toString - overridden, will be adjusted later
+    //===============================================================================================
 
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
         Vertex<E> currentVertex = firstVertex;
         while (currentVertex != null) {
-            sb.append(currentVertex.element);
-
-            sb.append('\n');
+          //  if (currentVertex.firstEdge != null){
+                sb.append(currentVertex.element).append(": ");
+                // do the edges here
+//                Edge<E> currentEdge = currentVertex.firstEdge;
+//                sb.append(currentEdge).append(" ");
+//                while(currentEdge.hasNext()){
+//                    sb.append(currentEdge.next()).append(" ");
+//                    currentEdge = currentEdge.next();
+//                }
+                sb.append('\n');
+       //     }
             currentVertex = currentVertex.next();
         }
-
-
         return sb.toString();
     }
 }
-
