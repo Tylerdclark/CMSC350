d68d415b77446e8106807c3f775df2a50735c41c
Tyler Clark <tylerc.thedeveloper@gmail.com>
2020-05-08 21:47:31 -0400

    This likely will not have changes

--- src/wk8/project4/Hierarchy.java
+++ src/wk8/project4/Hierarchy.java
@@ -1,5 +1,37 @@
 package wk8.project4;
 
-public class Hierarchy implements DFSActions{
-    //TODO: Overridden toString method
+public class Hierarchy<E> implements DFSActions<E>{
+
+    StringBuilder toPrint = new StringBuilder();
+    int indentCounter = 0;
+
+    @Override
+    public void cycleDetected() {
+        //admittedly lazy way to ensure the asterisk is not printed on the next line
+        toPrint.setLength(toPrint.length()-1); //deleting the \n added from processVertex()
+        toPrint.append(" * ").append("\n");
+
+    }
+
+    @Override
+    public void processVertex(E content) {
+        //either zero or some indent, so it repeats the max of counter or zero
+        toPrint.append(" ".repeat(Math.max(0, indentCounter)));
+        toPrint.append(content).append("\n");
+    }
+
+    @Override
+    public void descend() {
+        indentCounter += 4;
+    }
+
+    @Override
+    public void ascend() {
+        indentCounter -= 4;
+    }
+
+    @Override
+    public String toString() {
+        return toPrint.toString();
+    }
 }
