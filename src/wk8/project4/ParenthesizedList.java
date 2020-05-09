f9f591ab0bde375b212df518ab4f877ffdee31a2
Tyler Clark <tylerc.thedeveloper@gmail.com>
2020-05-08 21:50:17 -0400

    Will likely not need to change this

--- src/wk8/project4/ParenthesizedList.java
+++ src/wk8/project4/ParenthesizedList.java
@@ -1,5 +1,33 @@
 package wk8.project4;
 
-public class ParenthesizedList implements DFSActions{
-    //TODO: Overridden toString method
+public class ParenthesizedList<E> implements DFSActions<E>{
+
+    StringBuilder toPrint = new StringBuilder();
+
+    @Override
+    public void cycleDetected() {
+        toPrint.append(" * ");
+    }
+
+    @Override
+    public void processVertex(E content) {
+       toPrint.append(content);
+    }
+
+    @Override
+    public void descend() {
+        toPrint.append(" ( ");
+    }
+
+    @Override
+    public void ascend() {
+        toPrint.append(" ) ");
+    }
+
+    @Override
+    public String toString() {
+        String replaceString = toPrint.toString();
+        return replaceString.replace(" (  )", "").replace("(  *  )", " * ");
+    }
+
 }
