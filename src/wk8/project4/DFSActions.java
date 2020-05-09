bdc1aa9b51347eb22c53685e6a4fc2751ad77a3c
Tyler Clark <tylerc.thedeveloper@gmail.com>
2020-05-08 21:46:07 -0400

    This should be final

--- src/wk8/project4/DFSActions.java
+++ src/wk8/project4/DFSActions.java
@@ -1,6 +1,8 @@
 package wk8.project4;
 
-public interface DFSActions {
-    //TODO: It should contain four method signatures that correspond to the four actions performed in
-    // the depth first search: cycle detected, process vertex, descend and ascend.
+public interface DFSActions<E> {
+public void cycleDetected();
+public void processVertex(E content);
+public void descend();
+public void ascend();
 }
