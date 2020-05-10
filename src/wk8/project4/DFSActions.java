ad6490345d4dd2c8a440d93073889630c9bbecc8
Tyler Clark <tylerc.thedeveloper@gmail.com>
2020-05-09 23:51:16 -0400

    Documentation

--- src/wk8/project4/DFSActions.java
+++ src/wk8/project4/DFSActions.java
@@ -1,8 +1,17 @@
+/* File: DFSActions.java
+ * Date: 09 May 2020
+ * Author: Tyler D Clark
+ * Description: An interface that has method bodies for actions specific to locations in a Depth First Search. To be
+ * implemented by ParenthesizedList and Hierarchy */
+
 package wk8.project4;
 
 public interface DFSActions<E> {
-public void cycleDetected();
-public void processVertex(E content);
-public void descend();
-public void ascend();
+    void cycleDetected();
+
+    void processVertex(E content);
+
+    void descend();
+
+    void ascend();
 }
--- src/wk8/project4/Project4.java
+++ src/wk8/project4/Project4.java
@@ -1,3 +1,10 @@
+/* File: Project4.java
+ * Date: 09 May 2020
+ * Author: Tyler D Clark
+ * Description: Provides a JFileChooser for the user to select a file to be read in. A graph is then constructed from
+ * the provided file. The graph is then depth first search and printed as a parenthesized list and as a hierarchy
+ * the program will continue to prompt for files until the user chooses "No" or "Cancel" */
+
 package wk8.project4;
 
 import javax.swing.*;
@@ -9,17 +16,18 @@ import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 public class Project4 {
-
+    // Main here
     public static void main(String[] args) throws InvocationTargetException, InterruptedException {
         int userChoice = JOptionPane.showConfirmDialog(null, "Would you like to select a file?",
                 "Project 4", JOptionPane.YES_NO_CANCEL_OPTION);
-        while(userChoice == 0 ){
+        while (userChoice == 0) {
             //JFileChooser showDialog was giving me issues on OSX, so this allowed it to execute in the right thread
-            EventQueue.invokeAndWait(Project4::fromFile);//chose method reference over lambda expression
+            EventQueue.invokeAndWait(Project4::fromFile);
             userChoice = JOptionPane.showConfirmDialog(null, "Would you like to select a file?",
                     "Project 4", JOptionPane.YES_NO_CANCEL_OPTION);
         }
     }
+
     public static <E> void fromFile() {
         //Create JFileChooser
         JFileChooser chooser = new JFileChooser();
@@ -28,35 +36,39 @@ public class Project4 {
         //use current directory for ease
         chooser.setCurrentDirectory(new File("."));
         int response = chooser.showOpenDialog(chooser.getParent());
-        if (response == JFileChooser.APPROVE_OPTION){
+        if (response == JFileChooser.APPROVE_OPTION) {
             File file = chooser.getSelectedFile();
-            try { Scanner fileIn = new Scanner(file);
+            try {
+                Scanner fileIn = new Scanner(file);
                 //Instantiate graph
                 DirectedGraph<String> graph = new DirectedGraph<>();
                 //create graph
-                while (fileIn.hasNextLine()){ parseVertexString(fileIn.nextLine(), graph); }
+                while (fileIn.hasNextLine()) {
+                    parseVertexString(fileIn.nextLine(), graph);
+                }
                 //instantiate DFSActions objects
                 DFSActions<String> parenthesizedList = new ParenthesizedList<>();
-                DFSActions<String>  hierarchy = new Hierarchy<>();
+                DFSActions<String> hierarchy = new Hierarchy<>();
                 //do the DFS
-                //graph.performDFS(parenthesizedList);
+                graph.performDFS(parenthesizedList);
                 graph.performDFS(hierarchy);
                 //and print them
-                //System.out.println(parenthesizedList);
+                System.out.println(parenthesizedList + "\n");
                 System.out.println(hierarchy);
-                System.out.println(graph);
+                graph.unreachable();
+                System.out.println("\n" + graph);
 
-            }catch (NoSuchElementException nse){
-                JOptionPane.showMessageDialog(JOptionPane.getRootFrame(),"File is empty!");
-            }catch(FileNotFoundException fne){
-                JOptionPane.showMessageDialog(JOptionPane.getRootFrame(),"File is not found!");
+            } catch (NoSuchElementException nse) {
+                JOptionPane.showMessageDialog(JOptionPane.getRootFrame(), "File is empty!");
+            } catch (FileNotFoundException fne) {
+                JOptionPane.showMessageDialog(JOptionPane.getRootFrame(), "File is not found!");
             }
         }
     }
 
-    public static void parseVertexString(String fileLine, DirectedGraph<String> graph){
+    public static void parseVertexString(String fileLine, DirectedGraph<String> graph) {
         String[] toArray = fileLine.split(" ");
-        for (int i = 1; i < toArray.length; i++){
+        for (int i = 1; i < toArray.length; i++) {
             graph.createEdge(toArray[0], toArray[i]);
         }
     }
